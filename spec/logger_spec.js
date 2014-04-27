c016dbc2171c200d505e87874375b0fdd1cbb71c
Pedro Januário <prnjanuario@gmail.com>
2014-04-27 14:20:12 +0100

    added support for string formats on logger

--- spec/logger_spec.js
+++ spec/logger_spec.js
@@ -1,34 +1,63 @@
 describe('Logger', function() {
   var log = require('../lib/logger');
-  var message = "LOG MESSAGE";
 
-  it('trace level outputs log into console.trace', function(){
-    spyOn(console, 'trace');
-    log.trace(message);
-    expect(console.trace).toHaveBeenCalledWith(message);
+  it('trace level outputs log into console.log', function(){
+    spyOn(console, 'log');
+    log.trace("LOG MESSAGE");
+    expect(console.log).toHaveBeenCalledWith("LOG MESSAGE");
+  });
+
+  it('trace level outputs log into console.log with args', function(){
+    spyOn(console, 'log');
+    log.trace("LOG MESSAGE %s", 1);
+    expect(console.log).toHaveBeenCalledWith("LOG MESSAGE %s", 1);
   });
 
   it('debug level outputs log into console.log', function(){
     spyOn(console, 'log');
-    log.debug(message);
-    expect(console.log).toHaveBeenCalledWith(message);
+    log.debug("LOG MESSAGE");
+    expect(console.log).toHaveBeenCalledWith("LOG MESSAGE");
+  });
+
+  it('debug level outputs log into console.log with args', function(){
+    spyOn(console, 'log');
+    log.debug("LOG MESSAGE %s", 1);
+    expect(console.log).toHaveBeenCalledWith("LOG MESSAGE %s", 1);
   });
 
   it('info level outputs log into console.info', function(){
     spyOn(console, 'info');
-    log.info(message);
-    expect(console.info).toHaveBeenCalledWith(message);
+    log.info("LOG MESSAGE");
+    expect(console.info).toHaveBeenCalledWith("LOG MESSAGE");
+  });
+
+  it('info level outputs log into console.info with args', function(){
+    spyOn(console, 'info');
+    log.info("LOG MESSAGE %s", 1);
+    expect(console.info).toHaveBeenCalledWith("LOG MESSAGE %s", 1);
   });
 
   it('warn level outputs log into console.warn', function(){
     spyOn(console, 'warn');
-    log.warn(message);
-    expect(console.warn).toHaveBeenCalledWith(message);
+    log.warn("LOG MESSAGE");
+    expect(console.warn).toHaveBeenCalledWith("LOG MESSAGE");
+  });
+
+  it('warn level outputs log into console.warn with args', function(){
+    spyOn(console, 'warn');
+    log.warn("LOG MESSAGE %s", 1);
+    expect(console.warn).toHaveBeenCalledWith("LOG MESSAGE %s", 1);
   });
 
   it('error level outputs log into console.error', function(){
     spyOn(console, 'error');
-    log.error(message);
-    expect(console.error).toHaveBeenCalledWith(message);
+    log.error("LOG MESSAGE");
+    expect(console.error).toHaveBeenCalledWith("LOG MESSAGE");
+  });
+
+  it('error level outputs log into console.error with args', function(){
+    spyOn(console, 'error');
+    log.error("LOG MESSAGE %s", 1);
+    expect(console.error).toHaveBeenCalledWith("LOG MESSAGE %s", 1);
   });
 });
